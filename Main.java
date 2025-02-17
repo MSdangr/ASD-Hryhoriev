09a07ac4d2811460d48b8174f762bc7558bb467e
MSdangr <danya6841@knu.ua>
2025-02-17 13:15:56 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
 import java.util.Scanner;
 
-
 public class Main {
+
     public static void main(String[] args) {
-        Lab_1.main(args);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Вкажіть номер лабораторної роботи: ");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                Lab_1.main(args); 
+                break;
+            //case 2:
+                //Lab_2.main(args);
+                //break;
+            default:
+                System.out.println("Wrong number. Try again.");
+                break;
+        }
+
+        scanner.close();
     }
 }
